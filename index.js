80c84157512a1b696f95d1fb6fa8916660325b3b
janexmgd <janexmgd@gmail.com>
2024-06-01 11:36:43 +0700

    adding bypassing chq

--- index.js
+++ index.js
@@ -2,40 +2,70 @@ import fs from 'node:fs';
 import ora from 'ora';
 import axios from 'axios';
 import { setInterval } from 'node:timers/promises';
+import * as cheerio from 'cheerio';
 const getAccToken = async (query) => {
-  try {
-    const s = await axios.post(
-      'https://api.tapswap.ai/api/account/login',
-      {
-        init_data: query,
-        referrer: '',
-        bot_key: 'app_bot_0',
-      },
-      {
-        headers: {
-          'User-Agent':
-            'Mozilla/5.0 (Linux; Android 11; Redmi Note 8 Build/RQ3A.211001.001; wv) AppleWebKit/537.36 (KHTML, like Gecko) Version/4.0 Chrome/120.0.6099.144 Mobile Safari/537.36',
-          'Content-Type': 'application/json',
-          'sec-ch-ua':
-            '"Not_A Brand";v="8", "Chromium";v="120", "Android WebView";v="120"',
-          'x-app': 'tapswap_server',
-          'sec-ch-ua-mobile': '?1',
-          'x-cv': '608',
-          'sec-ch-ua-platform': '"Android"',
-          Origin: 'https://app.tapswap.club',
-          'X-Requested-With': 'org.telegram.messenger',
-          'Sec-Fetch-Site': 'cross-site',
-          'Sec-Fetch-Mode': 'cors',
-          'Sec-Fetch-Dest': 'empty',
-          Referer: 'https://app.tapswap.club/',
-          'Accept-Language': 'en,en-US;q=0.9',
-        },
+  console.log(`(,,>﹏<,,)`);
+  let attempts = 0;
+  let maxAttempts = 1;
+  let chq = '';
+  while (attempts <= maxAttempts) {
+    try {
+      let json;
+      if (!chq) {
+        json = {
+          init_data: query,
+          referrer: '',
+          bot_key: 'app_bot_0',
+        };
+      } else {
+        // source https://github.com/shamhi/TapSwapBot/blob/main/bot/core/tapper.py
+        json = {
+          chr: chq,
+          init_data: query,
+          referrer: '',
+          bot_key: 'app_bot_0',
+        };
       }
-    );
-    const data = await s.data;
-    return data;
-  } catch (error) {
-    throw error;
+      const { data } = await axios.post(
+        'https://api.tapswap.ai/api/account/login',
+        json,
+        {
+          headers: {
+            'User-Agent':
+              'Mozilla/5.0 (Linux; Android 11; Redmi Note 8 Build/RQ3A.211001.001; wv) AppleWebKit/537.36 (KHTML, like Gecko) Version/4.0 Chrome/120.0.6099.144 Mobile Safari/537.36',
+            'Content-Type': 'application/json',
+            'sec-ch-ua':
+              '"Not_A Brand";v="8", "Chromium";v="120", "Android WebView";v="120"',
+            'x-bot': 'no',
+            'sec-ch-ua-mobile': '?1',
+            'x-cv': '608',
+            'x-app': 'tapswap_server',
+            'sec-ch-ua-platform': '"Android"',
+            Origin: 'https://app.tapswap.club',
+            'X-Requested-With': 'app.mdgram.android',
+            'Sec-Fetch-Site': 'cross-site',
+            'Sec-Fetch-Mode': 'cors',
+            'Sec-Fetch-Dest': 'empty',
+            Referer: 'https://app.tapswap.club/',
+            'Accept-Language': 'en,en-US;q=0.9',
+          },
+        }
+      );
+
+      if (data.chq) {
+        chq = await extractChqResult(data.chq);
+      }
+      if (data.access_token) {
+        console.log(`success login`);
+        return data;
+      }
+    } catch (error) {
+      attempts += 1;
+      if (attempts >= maxAttempts) {
+        throw error;
+      }
+      console.log(`Attempt ${attempts} failed get acc token retrying...`);
+    }
   }
 };
 const padNumber = (num, length) => {
@@ -124,13 +154,59 @@ const boost = async (token, type) => {
       }
     );
     if (type == 'energy') {
-      console.log(`Succes claim energy refill`);
+      console.log(`Success claim energy refill`);
+      console.log();
     } else {
       console.log(`Succes claim turbo guru boost`);
       console.log(data.player.boost[1]);
+      console.log();
     }
 
     return data;
+  } catch (error) {
+    throw error;
+  }
+};
+const extractChqResult = async (chq) => {
+  try {
+    console.log(`bypassing chq 👉👈`);
+    const x = 157;
+    const len = chq.length;
+    const bytesArray = Buffer.alloc(len / 2);
+    // source https://github.com/Poryaei/TapSwap-Clicker/blob/main/tapswap.py
+    for (let t = 0; t < len; t += 2) {
+      bytesArray[t / 2] = parseInt(chq.slice(t, t + 2), 16);
+    }
+    const xored = Buffer.from(bytesArray.map((byte) => byte ^ x));
+    const decoded = xored.toString('utf-8');
+    const jsCode = decoded
+      .split(
+        'try {eval("document.getElementById");} catch {return 0xC0FEBABE;}'
+      )[1]
+      .split('\n');
+    let va_1 = jsCode[6].split('_')[1];
+    let va_2 = jsCode[6].split('_')[2].split('')[0];
+    let va = `_${va_1}_${va_2}`;
+    let vb_1 = jsCode[7].split('_')[1];
+    let vb_2 = jsCode[7].split('_')[2].split('')[0];
+    let vb = `_${vb_1}_${vb_2}`;
+    const randInt = jsCode[8].split(' ')[5].slice(0, -1);
+    const html = jsCode[5].split(' = ')[1].slice(0, -1);
+    const $ = cheerio.load(html);
+    const elementVa = $(`#${va}`);
+    if (elementVa) {
+      va = elementVa.attr('_d_');
+    } else {
+      throw new Error('failed get va');
+    }
+    const elementVb = $(`#${vb}`);
+    if (elementVb) {
+      vb = elementVb.attr('_d_');
+    } else {
+      throw new Error('failed get vb');
+    }
+    const chr = (parseInt(va) * parseInt(vb)) % parseInt(randInt);
+    return chr;
   } catch (error) {
     throw error;
   }
@@ -139,16 +215,14 @@ const boost = async (token, type) => {
   try {
     while (true) {
       process.stdout.write('\x1Bc');
+      console.log(`created with ♡ by janexmgd\n\n`);
       const reqTxt = fs.readFileSync('req.txt', 'utf-8').replace(/\r/g, '');
       const arrAcc = reqTxt.split('\n');
-      // return console.log(arrAcc);
       for (const acc of arrAcc) {
         const init_data = acc.split(' ')[0];
         const accInfo = await getAccToken(init_data);
         const { access_token, player } = accInfo;
-
         console.log(`Login as ${player.full_name}`);
-
         let energyRefill = player.boost[0].cnt;
         const playerid = player.id;
         while (energyRefill >= 0) {
@@ -190,7 +264,7 @@ const boost = async (token, type) => {
             }
           }
         }
-        console.log(`process done ${player.full_name} \n`);
+        console.log(`process done ${player.full_name} \n \n`);
       }
       const delayInSeconds = 300;
       let remainingSeconds = delayInSeconds;
@@ -199,7 +273,7 @@ const boost = async (token, type) => {
       const interval = 1000;
       for await (const startTime of setInterval(interval, Date.now())) {
         remainingSeconds -= 1;
-        spinner.text = `Delaying ${remainingSeconds} second`;
+        spinner.text = `Delaying ${remainingSeconds} second 🗿`;
         if (remainingSeconds <= 0) {
           clearInterval(interval);
           spinner.succeed('done');
---
+++ readme.md
@@ -0,0 +1,22 @@
+# tapswap-click
+
+this for auto click at telegran game <a href='https://t.me/tapswap_mirror_2_bot?start=r_2070067098'>tapswap</a>
+
+<b>Warning: Performing this action may result in your Telegram or game account being banned. By proceeding, you acknowledge that the script creator is not responsible for any consequences.</b>
+
+# How to run
+
+setup your hash query request at req.txt (make new file),
+you can get it from request post https://api.tapswap.ai/api/account/login and see the request data, you can change your user agent first to android
+
+example hash
+
+```
+query_id=YOUROWN
+```
+
+dont forget
+
+```
+npm install
+```
